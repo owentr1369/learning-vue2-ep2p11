549dde8b3891faa83cec177a832695128f44d644
owentr1369 <tamvaa1306@gmail.com>
2022-05-25 20:55:53 +0700

    ep2 part 12

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <div id="app" v-on:mouseover="handlePosition">
-    <h1>HelloWorld</h1>
-    <div id="obj">
-      <h1>Bam nut de tang gia tri</h1>
-      <h2>{{ dem }}</h2>
-      <button v-on:click="handleIncrease">Increase1</button>
-      <button v-on:click="dem++">Increase2</button>
-      <p>Toa do cua chuot: {{ x }} : {{ y }}</p>
-      <p>Gia tri: {{ dem > 20 ? "qua 20 r" : "chua qua 20" }}</p>
-      <span v-on:mouseover="handleMouseOver">Dung su kien</span>
-      <!-- <span v-on:mouseover.stop="">Dung su kien</span> -->
-    </div>
-    <div class="hello">
-      <input type="text" v-on:keyup.space.enter="handleKeyboard" />
-    </div>
+  <div>
+    <h1>Hello</h1>
+    <input type="text" v-model="name" />
+    <p>{{ name }}</p>
   </div>
 </template>
 
@@ -22,26 +11,10 @@ export default {
   name: "App",
   data() {
     return {
-      dem: 0,
-      x: 0,
-      y: 0,
+      name: "Tam",
     };
   },
-  methods: {
-    handleIncrease: function () {
-      this.dem = this.dem + 1;
-    },
-    handlePosition: function (event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    handleMouseOver: function (event) {
-      event.stopPropagation();
-    },
-    handleKeyboard: function () {
-      alert("Helloworld");
-    },
-  },
+  methods: {},
 };
 </script>
 
